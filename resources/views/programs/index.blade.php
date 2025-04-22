ea2e4f4c79592c9d844d836715d4692d1e532b0f
Kit Ying Tong (Tommie) <tongky0704@outlook.com>
2025-04-22 13:24:08 -0400

    Style the programs index

--- resources/views/programs/index.blade.php
+++ resources/views/programs/index.blade.php
@@ -7,6 +7,7 @@
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="/app.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" integrity="sha384-tViUnnbYAV00FLIhhi3v/dWt3Jxw4gZQcNoSCxCIFNJVCx7/D55/wXsrNIRANwdD" crossorigin="anonymous">
         <script src="/app.js"></script>        
     </head>
     <body>
@@ -50,7 +51,7 @@
             </div>
         </nav>
 
-        <div class="container mt-4">
+        <main class="container mt-4">
             @if(session()->has('message'))
                 <div class="alert alert-success">
                     {{ session('message') }}
@@ -58,10 +59,47 @@
             @endif
 
             <div class="d-flex mb-2">
-                <h2>Manage Programs</h2>
+                <h1>Manage Programs</h1>
             </div>
-            <a href="{{ route('programs.create')}}" class="btn btn-danger">New Program</a>
-            <div class="row mt-3">
+            <a href="{{ route('programs.create')}}" class="btn btn-success mb-4">New Program</a>
+
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                @foreach($programs as $program)
+                    <div class="col">
+                        <div class="card h-100">
+                            <h1 class="card-header bg-warning-subtle fs-3">{{ $program->title }}</h1>
+
+                            <div class="pt-4 text-center">
+                            @if ($program->image)
+                                <img src="{{ asset('storage/' . $program->image) }}" class="card-img-top d-block mx-auto" style="max-height: 14rem; width: auto;" alt="Program Image">
+                            @else
+                                <i class="bi bi-card-image" style="font-size:6rem"></i>
+                            @endif
+                            </div>
+
+                            <div class="card-body text-center">
+                                <p class="card-title">${{ $program->price }}</p>
+                            </div>
+
+                            <div class="card-footer">
+                                <small class="text-body-secondary d-flex justify-content-evenly align-items-center gap-2">
+                                    <a href="{{ route('programs.show', $program->id) }}" class="btn btn-info btn-sm">View</a>
+                                    <a href="{{ route('programs.edit', $program->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                                    <form action="{{ route('programs.destroy', $program->id) }}" method="POST" class="d-inline">
+                                        {{ csrf_field() }}
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this program?')">Delete</button>
+                                    </form>
+                                </small>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach                
+            </div>
+        </main>
+
+
+            {{-- <div class="row mt-3">
                 @foreach($programs as $program)
                     <div class="col-sm-4">
                         <div class="card shadow-lg rounded-4 p-4 mt-4" style="min-height: 580px;">
@@ -88,10 +126,7 @@
                             </div>
                         </div>
                     </div>
-                @endforeach
-            </div>
-            
-        </div>
+            </div>     --}}
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
